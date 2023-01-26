fccc3e83bf78f42639e22fbd0c5dd64a176fa196
Junxian Liu <liujunxian134@gmail.com>
2023-01-26 15:39:32 -0800

    update

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,28 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
-
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReverseInPlaceLarge(){
+    int[] input = {1, 2, 3};
+    ArrayExamples.reverseInPlace(input);
+    assertArrayEquals(new int[]{3, 2, 1}, input);
+  }
+
+  @Test
+  public void testReversedNew(){
+    int[] input = {1, 2, 3};
+    assertArrayEquals(new int[]{3, 2, 1}, ArrayExamples.reversed(input));
+  }
+
+  @Test
+  public void testAverage(){
+    double[] input = {1, 1, 1, 1};
+    assertEquals(1, (int)(ArrayExamples.averageWithoutLowest(input)));
   }
 }
